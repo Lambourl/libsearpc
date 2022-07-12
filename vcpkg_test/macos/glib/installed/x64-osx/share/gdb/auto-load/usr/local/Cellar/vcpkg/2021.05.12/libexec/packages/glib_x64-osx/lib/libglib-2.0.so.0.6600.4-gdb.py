2b694115559b1353b4f615989c9a007a4155b0de
Lambourl <lmbrluc@gmail.com>
2022-07-12 12:03:47 +0200

    :wink: add glib with vcpkg

---
+++ vcpkg_test/macos/glib/installed/x64-osx/share/gdb/auto-load/usr/local/Cellar/vcpkg/2021.05.12/libexec/packages/glib_x64-osx/lib/libglib-2.0.so.0.6600.4-gdb.py
@@ -0,0 +1,10 @@
+import sys
+import gdb
+
+# Update module path.
+dir_ = '/usr/local/Cellar/vcpkg/2021.05.12/libexec/packages/glib_x64-osx/share/glib-2.0/gdb'
+if not dir_ in sys.path:
+    sys.path.insert(0, dir_)
+
+from glib_gdb import register
+register (gdb.current_objfile ())
